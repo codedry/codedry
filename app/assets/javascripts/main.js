60aad8fefff8e1b8b0997695813e22c630313e1b
Marlon Williams <marlon.willi@gmail.com>
2014-10-08 14:25:06 -0500

    GH-1 Add new line to end of file

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
     /* ======= jQuery Placeholder ======= */
-    $('input, textarea').placeholder();    
-    
+    $('input, textarea').placeholder();
+
     /* ======= jQuery FitVids - Responsive Video ======= */
     $(".video-container").fitVids();
-    
+
     /* ======= FAQ accordion ======= */
 
     function toggleIcon(e) {
@@ -18,9 +18,9 @@ $(document).ready(function() {
     }
     $('.panel').on('hidden.bs.collapse', toggleIcon);
     $('.panel').on('shown.bs.collapse', toggleIcon);
-    
+
     /* ======= Fixed header when scrolled ======= */
-    
+
     $(window).bind('scroll', function() {
          if ($(window).scrollTop() > 50) {
              $('#header').addClass('navbar-fixed-top');
@@ -29,38 +29,38 @@ $(document).ready(function() {
              $('#header').removeClass('navbar-fixed-top');
          }
     });
-    
-    
-    
+
+
+
     /* ======= Style Switcher ======= */
-    
+
     $('#config-trigger').on('click', function(e) {
         var $panel = $('#config-panel');
         var panelVisible = $('#config-panel').is(':visible');
         if (panelVisible) {
-            $panel.hide();          
+            $panel.hide();
         } else {
             $panel.show();
         }
         e.preventDefault();
     });
-    
+
     $('#config-close').on('click', function(e) {
         e.preventDefault();
         $('#config-panel').hide();
     });
-    
-    
-    $('#color-options a').on('click', function(e) { 
+
+
+    $('#color-options a').on('click', function(e) {
         var $styleSheet = $(this).attr('data-style');
-		$('#theme-style').attr('href', $styleSheet);	
-				
+		$('#theme-style').attr('href', $styleSheet);
+
 		var $listItem = $(this).closest('li');
 		$listItem.addClass('active');
 		$listItem.siblings().removeClass('active');
-		
+
 		e.preventDefault();
-		
+
 	});
 
 
