5ca80dcdf1e452feca1f244fae23c503f05ba800
Marcel Morgan <marcel.morgan@codedry.com>
2014-10-09 09:39:40 -0500

    Validate format of email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :name, :email, :message
+  validates :email, email: true, allow_blank: true
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -8,7 +8,15 @@ class ContactTest < ActiveSupport::TestCase
   %w(name email message).each do |required_field|
     test "validates presence of #{required_field}" do
       @contact.validate
-      assert_equal @contact.errors[required_field], ["can't be blank"]
+      assert_equal ["can't be blank"], @contact.errors[required_field]
+    end
+  end
+
+  %w(invalid invalid@ invalid@x invalid@x.).each do |invalid_email|
+    test "reject invalid email '#{invalid_email}'" do
+      @contact.email = invalid_email
+      @contact.validate
+      assert_equal ["is not an email"], @contact.errors[:email]
     end
   end
 end
