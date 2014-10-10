b0697ae46640e8701f6b53fe365ae719e52439ae
Marcel Morgan <marcel.morgan@codedry.com>
2014-10-10 08:20:33 -0500

    Add preview for contact received acknowledgement email

--- test/mailers/previews/notifier_preview.rb
+++ test/mailers/previews/notifier_preview.rb
@@ -3,4 +3,8 @@ class NotifierPreview < ActionMailer::Preview
   def contact_received
     Notifier.contact_received(Contact.first)
   end
+
+  def contact_received_acknowledgement
+    Notifier.contact_received_acknowledgement(Contact.first)
+  end
 end
